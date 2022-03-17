26ef330d72abbc3dad01d85328493edac6b281b8
Jon Cram <webignition@gmail.com>
2022-03-17 10:17:05 +0000

    Tidy up

--- src/Parser.php
+++ src/Parser.php
@@ -22,39 +22,25 @@ class Parser
 
         $parsedDocuments = [];
         $lines = explode("\n", $yaml);
+        $lineCount = count($lines);
 
         $currentLines = [];
 
-        foreach ($lines as $line) {
+        foreach ($lines as $lineIndex => $line) {
             $isDocumentStart = Document::isDocumentStart($line);
             $isDocumentEnd = Document::isDocumentEnd($line);
-
-            if ($isDocumentStart) {
-                if ([] !== $currentLines) {
-                    $parsedDocuments[] = $this->createParsedDocument($currentLines);
-                    $currentLines = [];
-                }
-            }
+            $isLastLine = $lineIndex === $lineCount - 1;
 
             if (false === $isDocumentStart && false === $isDocumentEnd) {
                 $currentLines[] = $line;
             }
-        }
 
-        if ([] !== $currentLines) {
-            $parsedDocuments[] = $this->createParsedDocument($currentLines);
+            if (($isDocumentStart || $isLastLine) && [] !== $currentLines) {
+                $parsedDocuments[] = (new Document(implode("\n", $currentLines)))->parse();
+                $currentLines = [];
+            }
         }
 
         return $parsedDocuments;
-    }
-
-    /**
-     * @param string[] $lines
-     *
-     * @throws ParseException
-     */
-    private function createParsedDocument(array $lines): mixed
-    {
-        return (new Document(implode("\n", $lines)))->parse();
     }
 }

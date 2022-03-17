2b77425bf270b78dd64400a189bfeb82fc3ccf5a
Jon Cram <webignition@gmail.com>
2022-03-17 10:12:29 +0000

    Don't trim yaml content when parsing

--- src/Parser.php
+++ src/Parser.php
@@ -4,46 +4,57 @@ declare(strict_types=1);
 
 namespace webignition\YamlDocumentSetParser;
 
+use Symfony\Component\Yaml\Exception\ParseException;
 use webignition\YamlDocument\Document;
 
 class Parser
 {
     /**
+     * @throws ParseException
+     *
      * @return array<mixed>
      */
     public function parse(string $yaml): array
     {
-        $yaml = trim($yaml);
-        if ('' === $yaml) {
+        if ('' === trim($yaml)) {
             return [];
         }
 
         $parsedDocuments = [];
-        $currentDocument = new Document();
-
         $lines = explode("\n", $yaml);
+
+        $currentLines = [];
 
         foreach ($lines as $line) {
             $isDocumentStart = Document::isDocumentStart($line);
             $isDocumentEnd = Document::isDocumentEnd($line);
 
             if ($isDocumentStart) {
-                if (false === $currentDocument->isEmpty()) {
-                    $parsedDocuments[] = $currentDocument->parse();
+                if ([] !== $currentLines) {
+                    $parsedDocuments[] = $this->createParsedDocument($currentLines);
+                    $currentLines = [];
                 }
-
-                $currentDocument = new Document();
             }
 
             if (false === $isDocumentStart && false === $isDocumentEnd) {
-                $currentDocument = $currentDocument->append("\n" . $line);
+                $currentLines[] = $line;
             }
         }
 
-        if (false === $currentDocument->isEmpty()) {
-            $parsedDocuments[] = $currentDocument->parse();
+        if ([] !== $currentLines) {
+            $parsedDocuments[] = $this->createParsedDocument($currentLines);
         }
 
         return $parsedDocuments;
+    }
+
+    /**
+     * @param string[] $lines
+     *
+     * @throws ParseException
+     */
+    private function createParsedDocument(array $lines): mixed
+    {
+        return (new Document(implode("\n", $lines)))->parse();
     }
 }
